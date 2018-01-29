ae9e068e70bcdc57e80dbb2e4a921757d4c25ab1
Kasra Bigdeli <kasra85@gmail.com>
2018-01-28 20:49:36 -0800

    Some minor bugfixes from PR

--- app-backend/src/user/ServiceManager.js
+++ app-backend/src/user/ServiceManager.js
@@ -449,20 +449,24 @@ class ServiceManager {
 
                 let rootDomain = self.dataStore.getRootDomain();
                 let dotRootDomain = "." + rootDomain;
-                
-                if (!(customDomain || /^[a-z0-9\-\.]+$/.test(customDomain))) {
+
+                if (!customDomain || !(/^[a-z0-9\-\.]+$/.test(customDomain))) {
                     throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_BAD_NAME,
                         'Domain name is not accepted. Please use alphanumerical domains such as myapp.google123.ca');
                 }
+
                 if (customDomain.length > 80) {
                     throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_BAD_NAME,
                         'Domain name is not accepted. Please use alphanumerical domains less than 80 characters in length.');
                 }
+
                 if (customDomain.indexOf('..') >= 0) {
                     throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_BAD_NAME,
                         'Domain name is not accepted. You cannot have two consecutive periods ".." inside a domain name. Please use alphanumerical domains such as myapp.google123.ca');
                 }
-                if (!(customDomain.indexOf(dotRootDomain) < 0 || (customDomain === rootDomain) || ((customDomain.indexOf(rootDomain) + rootDomain.length) !== customDomain.length))) {
+
+                if (customDomain.indexOf(dotRootDomain) >= 0
+                    && (customDomain.indexOf(dotRootDomain) + dotRootDomain.length) === customDomain.length) {
                     throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_BAD_NAME,
                         'Domain name is not accepted. Custom domain cannot be subdomain of root domain.');
                 }
