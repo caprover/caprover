43808e2177aaca0d8ebec65e49a426863a7a2ec8
Kasra Bigdeli <kasra85@gmail.com>
2018-11-19 22:09:54 -0800

    second piece of datastore migrated

--- app-backend/src/datastore/DataStoreProvider.ts
+++ app-backend/src/datastore/DataStoreProvider.ts
@@ -1,18 +1,24 @@
-"use strict";
 /**
  * Created by kasra on 27/06/17.
  */
-const DataStore = require("./DataStoreImpl");
-const dataStoreCache = {};
-module.exports = {
-    getDataStore: function (namespace) {
+
+import DataStore = require("./DataStoreImpl");
+
+interface ICache {
+    [namespace: string]: DataStore;
+}
+const dataStoreCache: ICache = {};
+
+export = {
+    getDataStore: function(namespace: string) {
         if (!namespace) {
             throw new Error("NameSpace is empty");
         }
+
         if (!dataStoreCache[namespace]) {
             dataStoreCache[namespace] = new DataStore(namespace);
         }
+
         return dataStoreCache[namespace];
     },
 };
-//# sourceMappingURL=DataStoreProvider.js.map
