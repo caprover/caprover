cffc5614eee6eb5850fd897df7f5000d8ef8ab90
Kasra Bigdeli <kasra85@gmail.com>
2018-11-19 21:37:12 -0800

    Template Helper moved

--- app-backend/src/user/TemplateHelper.ts
+++ app-backend/src/user/TemplateHelper.ts
@@ -1,58 +1,73 @@
-"use strict";
-const request = require("request");
-const fs = require("fs-extra");
-const ApiStatusCodes = require("../api/ApiStatusCodes");
-const Logger = require("../utils/Logger");
-const CaptainConstants = require("../utils/CaptainConstants");
-function getTagsForImage(imageBaseName, url, allTags) {
+import request = require("request");
+import fs = require("fs-extra");
+import ApiStatusCodes = require("../api/ApiStatusCodes");
+import Logger = require("../utils/Logger");
+import CaptainConstants = require("../utils/CaptainConstants");
+
+function getTagsForImage(imageBaseName: string, url: string|undefined, allTags: string[]|undefined): Promise<string[]> {
+
     if (!url) {
         url = "https://hub.docker.com/v2/repositories/" + imageBaseName + "/tags";
     }
+
     return new Promise(function (resolve, reject) {
-        request(url, function (error, response, body) {
-            if (error || !body) {
-                Logger.e(error);
-                reject(error);
-                return;
-            }
-            try {
-                // Sometimes Docker server is down and it crashes Captain!
-                body = JSON.parse(body);
-            }
-            catch (e) {
-                Logger.e(e);
-            }
-            let results;
-            if (body) {
-                results = body.results;
-            }
-            if (!results) {
-                Logger.e("NO RESULT");
-                reject(new Error("NO RESULT"));
-                return;
-            }
-            if (!allTags) {
-                allTags = [];
-            }
-            for (let idx = 0; idx < results.length; idx++) {
-                allTags.push(results[idx].name);
-            }
-            if (body.next) {
-                resolve(getTagsForImage(imageBaseName, body.next, allTags));
-                return;
-            }
-            resolve(allTags);
-        });
+
+        request(url!,
+
+            function (error, response, body) {
+
+                if (error || !body) {
+                    Logger.e(error);
+                    reject(error);
+                    return;
+                }
+
+                try {
+                    // Sometimes Docker server is down and it crashes Captain!
+                    body = JSON.parse(body);
+                } catch (e) {
+                    Logger.e(e);
+                }
+
+                let results: any;
+
+                if (body) {
+                    results = body.results;
+                }
+
+                if (!results) {
+                    Logger.e("NO RESULT");
+                    reject(new Error("NO RESULT"));
+                    return;
+                }
+
+                if (!allTags) {
+                    allTags = [];
+                }
+
+                for (let idx = 0; idx < results.length; idx++) {
+                    allTags.push(results[idx].name);
+                }
+
+                if (body.next) {
+                    resolve(getTagsForImage(imageBaseName, body.next, allTags));
+                    return;
+                }
+
+                resolve(allTags);
+            });
     });
 }
-function firstEndsWithSecond(str1, str2) {
+
+function firstEndsWithSecond(str1: string, str2: string) {
     if (!str1 || !str2) {
         throw new Error("Str1 or Str2 are null " + !str1 + " " + !str2);
     }
     const idx = str1.indexOf(str2);
     return idx >= 0 && (idx + str2.length) === str1.length;
 }
-function isEmpty(obj) {
+
+function isEmpty(obj: any) {
     for (const key in obj) {
         if (obj.hasOwnProperty(key)) {
             return false;
@@ -60,9 +75,28 @@ function isEmpty(obj) {
     }
     return true;
 }
+
+interface ITemplate {
+    templateName: string;
+    dockerHubImageName: string;
+    dockerFileFromName: string;
+    displayName: string;
+    tagSuffix: string;
+    postFromLines?: string;
+}
+
+
+interface ICache {
+    [id: string]: any[];
+}
+
 class TemplateHelper {
+
+    private templates: ITemplate[];
+    private cachedImageTags: ICache;
     constructor() {
-        const templates = [{
+
+        const templates: ITemplate[] = [{
                 templateName: "node",
                 dockerHubImageName: "library/node",
                 dockerFileFromName: "node",
@@ -91,23 +125,33 @@ class TemplateHelper {
                 tagSuffix: "-alpine3.7"
             }
         ];
+
         const dockerfilesRoot = __dirname + "/../../dockerfiles/";
+
         for (let i = 0; i < templates.length; i++) {
             templates[i].postFromLines = fs.readFileSync(dockerfilesRoot + templates[i].templateName, "utf8");
         }
+
         this.templates = templates;
         this.cachedImageTags = {};
+
         if (CaptainConstants.isDebug) {
             this.printAvailableImageTagsForReadme();
         }
     }
-    getDockerVersionsForTemplateName(templateName) {
+
+    getDockerVersionsForTemplateName(templateName: string) {
+
         const self = this;
+
         const templateObj = self.getTemplateFromTemplateName(templateName);
+
         if (isEmpty(this.cachedImageTags)) {
             throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_GENERIC, "Please wait about 30 seconds, then try again.");
         }
+
         const tags = self.cachedImageTags[templateObj.dockerHubImageName];
+
         const dockerVersions = [];
         for (let i = 0; i < tags.length; i++) {
             const t = tags[i];
@@ -115,42 +159,59 @@ class TemplateHelper {
                 dockerVersions.push(t.substring(0, t.length - templateObj.tagSuffix.length));
             }
         }
+
         return dockerVersions;
     }
+
     printAvailableImageTagsForReadme() {
+
         const self = this;
         self.cachedImageTags = {};
-        const tempCache = {};
+
+        const tempCache: ICache = {};
         for (let i = 0; i < self.templates.length; i++) {
+
             const currentImageName = self.templates[i].dockerHubImageName;
+
             getTagsForImage(currentImageName, undefined, undefined)
                 .then(function (tags) {
-                tempCache[currentImageName] = tags;
-                let isAllDone = true;
-                for (let j = 0; j < self.templates.length; j++) {
-                    const imageName = self.templates[j].dockerHubImageName;
-                    if (!tempCache[imageName]) {
-                        isAllDone = false;
+
+                    tempCache[currentImageName] = tags;
+
+                    let isAllDone = true;
+
+                    for (let j = 0; j < self.templates.length; j++) {
+                        const imageName = self.templates[j].dockerHubImageName;
+                        if (!tempCache[imageName]) {
+                            isAllDone = false;
+                        }
                     }
-                }
-                if (isAllDone) {
-                    Logger.d("Template Cache Updated!");
-                    self.cachedImageTags = tempCache;
-                    // Used for README
-                    for (let tempIdx = 0; tempIdx < self.templates.length; tempIdx++) {
-                        Logger.d(" ");
-                        Logger.d(self.templates[tempIdx].templateName + "/");
-                        Logger.d(self.getDockerVersionsForTemplateName(self.templates[tempIdx].templateName).join(", "));
-                        Logger.d(" ");
+
+                    if (isAllDone) {
+
+                        Logger.d("Template Cache Updated!");
+
+                        self.cachedImageTags = tempCache;
+
+                        // Used for README
+                        for (let tempIdx = 0; tempIdx < self.templates.length; tempIdx++) {
+                            Logger.d(" ");
+                            Logger.d(self.templates[tempIdx].templateName + "/");
+                            Logger.d(self.getDockerVersionsForTemplateName(self.templates[tempIdx].templateName).join(", "));
+                            Logger.d(" ");
+                        }
                     }
-                }
-            })
+
+                })
                 .catch(function (error) {
-                Logger.e(error);
-            });
+
+                    Logger.e(error);
+
+                });
         }
     }
-    getTemplateFromTemplateName(templateName) {
+
+    getTemplateFromTemplateName(templateName: string) {
         for (let i = 0; i < this.templates.length; i++) {
             if (this.templates[i].templateName === templateName) {
                 return this.templates[i];
@@ -158,22 +219,28 @@ class TemplateHelper {
         }
         throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_GENERIC, "TEMPLATE NAME NOT FOUND: " + templateName);
     }
-    getDockerfileContentFromTemplateTag(templateAndVersion) {
+
+    getDockerfileContentFromTemplateTag(templateAndVersion: string) {
         const self = this;
         const templateName = templateAndVersion.split("/")[0];
         const templateVersion = templateAndVersion.split("/")[1];
         if (!templateVersion) {
-            throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_GENERIC, "Template version field is empty!");
+            throw ApiStatusCodes.createError(ApiStatusCodes.STATUS_ERROR_GENERIC,
+                "Template version field is empty!");
         }
+
         const templateObj = self.getTemplateFromTemplateName(templateName);
+
         const fromLine = templateObj.dockerHubImageName + ":" + templateVersion + templateObj.tagSuffix;
+
         return "FROM " + fromLine + "\n" + templateObj.postFromLines;
     }
 }
+
 const templateHelperInstance = new TemplateHelper();
-module.exports = {
+
+export = {
     get: function () {
         return templateHelperInstance;
     }
 };
-//# sourceMappingURL=TemplateHelper.js.map
