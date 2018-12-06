d1470ccbfd60b1772ec9ef6fb58e1a76b2ba21d2
Kasra Bigdeli <kasra85@gmail.com>
2018-12-05 22:20:22 -0800

    block no namespace for api

--- app-backend/built/app.js
+++ app-backend/built/app.js
@@ -118,6 +118,11 @@ app.use(API_PREFIX + ':apiVersionFromRequest/', function (req, res, next) {
         res.send(response);
         return;
     }
+    if (!InjectionExtractor.extractGlobalsFromInjected(res).namespace) {
+        let response = new BaseApi(ApiStatusCodes.STATUS_ERROR_GENERIC, 'No namespace is defined');
+        res.send(response);
+        return;
+    }
     next();
 });
 // unsecured end points:
