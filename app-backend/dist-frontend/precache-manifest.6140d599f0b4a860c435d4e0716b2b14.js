9a1d7aed2e8e209cdd36187875368e3d47a84573
Kasra Bigdeli <kasra85@gmail.com>
2018-12-29 19:38:41 -0800

    Added new frontend

---
+++ app-backend/dist-frontend/precache-manifest.6140d599f0b4a860c435d4e0716b2b14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a3f5d174e080c9e594c4",
+    "url": "/static/js/main.a3f5d174.chunk.js"
+  },
+  {
+    "revision": "9f66f9618caf929bfb9f",
+    "url": "/static/js/1.9f66f961.chunk.js"
+  },
+  {
+    "revision": "a3f5d174e080c9e594c4",
+    "url": "/static/css/main.a4fb059b.chunk.css"
+  },
+  {
+    "revision": "9f66f9618caf929bfb9f",
+    "url": "/static/css/1.a968d7ed.chunk.css"
+  },
+  {
+    "revision": "eccf3828840f8021dbdd3b8f39c94b99",
+    "url": "/index.html"
+  }
+];
