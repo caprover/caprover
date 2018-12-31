7c942f8d813177a7c895eb35754b1450dbb0f318
Kasra Bigdeli <kasra85@gmail.com>
2018-12-30 19:12:44 -0800

    rebuilt frontend

--- app-backend/dist-frontend/service-worker.js
+++ app-backend/dist-frontend/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48e75c374a53d38cfa518a97663959d3.js"
+  "/precache-manifest.2559f7b34b23a9d2d4313765151dbcc0.js"
 );
 
 workbox.clientsClaim();
