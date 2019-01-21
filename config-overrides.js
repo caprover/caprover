4912b70e4a2986cf6ff5296d1a7de89088fb55e7
Kasra Bigdeli <kasra85@gmail.com>
2019-01-20 17:43:59 -0800

    Change dependency management to npm as it is being used by dockerfiles. Fixed bug

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,20 @@
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-module.exports = function override(config, env) {
-    // config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config);  // change importing css to less
-    config = rewireLess.withLoaderOptions({
+const {
+    override,
+    fixBabelImports,
+    addLessLoader,
+} = require("customize-cra");
+
+module.exports = override(
+    fixBabelImports("import", {
+        libraryName: "antd",
+        libraryDirectory: "es",
+        style: true // change importing css to less
+    }),
+    addLessLoader({
         javascriptEnabled: true,
         modifyVars: {
             "@primary-color": "#1b8ad3",
-            '@font-family' : '"Google Sans", "Chinese Quote", -apple-system, BlinkMacSystemFont, "Segoe UI", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial, sans-serif',
+            '@font-family': '"Google Sans", "Chinese Quote", -apple-system, BlinkMacSystemFont, "Segoe UI", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial, sans-serif',
         },
-    })(config, env);
-    return config;
-};
+    })
+);
