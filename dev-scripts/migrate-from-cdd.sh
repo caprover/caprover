ef65ebc0d0ebe6faadb1ff015d984afe18fa5b6f
Kasra Bigdeli <kasra85@gmail.com>
2019-01-30 21:11:28 -0800

    Added untested migration script

---
+++ dev-scripts/migrate-from-cdd.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Run this script on the server to migrate from CaptainDuckDuck.
+################## EDGE VERSION ##### UNTESTED #################
+
+cd / && \
+docker service scale captain-captain=0 && \
+docker service rm captain-certbot captain-nginx && \
+(docker service rm captain-registry || true) && \
+sleep 3s && echo "waiting..." && sleep 3s && echo "waiting..." && \
+rm -rf /captain/generated && rm -rf /captain/temp && \
+tar -czvf /captain-bk-$(date +%Y_%m_%d_%H_%M_%S).tar /captain && \
+mkdir -p /captain/data && \
+mv /captain/letencrypt /captain/data/ && \
+mv /captain/nginx-shared /captain/data/ && \
+mv /captain/registry /captain/data/ && \
+mv /captain/config.conf /captain/data/ && \
+docker pull caprover/caprover-edge && \
+docker service update --image caprover/caprover-edge captain-captain && \
+docker service scale captain-captain=1 --detach && \
+docker service logs captain-captain --follow
