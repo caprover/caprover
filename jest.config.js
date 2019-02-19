16922feaf2d80f009382cdc05e94a724c6af6764
Kasra Bigdeli <kasra85@gmail.com>
2019-02-18 17:07:42 -0800

    updated test dir

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
         "^.+\\.tsx?$": "ts-jest",
         "^.+\\.ts?$": "ts-jest",
     },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?|ts?|js?)$",
+    testRegex: "(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?|ts?|js?)$",
     testPathIgnorePatterns: ["/node_modules/"],
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverage: true,
