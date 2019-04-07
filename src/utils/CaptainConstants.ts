192cac75f23a2dadf80fe4e24a43e007e02827b3
Kasra Bigdeli <kasra85@gmail.com>
2019-04-07 11:41:07 -0700

    Changed version to 1.4.0

--- src/utils/CaptainConstants.ts
+++ src/utils/CaptainConstants.ts
@@ -17,7 +17,7 @@ const CONSTANT_FILE_OVERRIDE_USER =
 const configs = {
     publishedNameOnDockerHub: 'caprover/caprover',
 
-    version: '1.3.0',
+    version: '1.4.0',
 
     defaultMaxLogSize: '512m',
 
