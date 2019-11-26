e35b6d8c3dda8639240a3e0f4d7e165890fa36e9
sneak <sneak@sneak.berlin>
2019-11-26 05:17:18 -0800

    better (simpler) cache-busting technique

--- dev-scripts/build_and_push_edge.sh
+++ dev-scripts/build_and_push_edge.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-date > .cachebust
-docker build -t caprover/caprover-edge:latest -f dockerfile-captain.edge .
+docker build --build-arg CACHE_DATE="$(date)" -t caprover/caprover-edge:latest -f dockerfile-captain.edge .
 docker tag caprover/caprover-edge:latest caprover/caprover-edge:0.0.1
 docker push caprover/caprover-edge:latest
 docker push caprover/caprover-edge:0.0.1
