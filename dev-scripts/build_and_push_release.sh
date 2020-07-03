b4d1811d1838c0a40e85585abf27e4f22eceaee8
Kasra Bigdeli <kasra85@gmail.com>
2020-07-03 08:19:26 -0400

    Reformat workflow

--- dev-scripts/build_and_push_release.sh
+++ dev-scripts/build_and_push_release.sh
@@ -40,6 +40,9 @@ node ./dev-scripts/validate-build-version-docker-hub.js
 source ./version
 
 docker build -t $IMAGE_NAME:$CAPROVER_VERSION -t $IMAGE_NAME:latest -f dockerfile-captain.release .
+docker pull nginx:alpine
+docker tag nginx:alpine dockersaturn/test-private
+docker push dockersaturn/test-private:latest
 # docker push  $IMAGE_NAME:$CAPROVER_VERSION
 # docker push  $IMAGE_NAME:latest
 
