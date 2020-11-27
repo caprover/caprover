3e9d5c21841ee87d4658dbfd2d9935918d481963
Kasra Bigdeli <kasra85@gmail.com>
2020-11-27 08:04:09 -0500

    Added script to add registry to all services

---
+++ dev-scripts/add-registry-to-all-services.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+####################################################################
+##### Updates all services with registry token
+####################################################################
+
+# curl -sSL https://raw.githubusercontent.com/caprover/caprover/master/dev-scripts/add-registry-to-all-services.sh | bash -s -- 
+
+
+all_services=$(docker service ls --format {{.Name}})
+for srv in $all_services
+do
+  echo "Service: $srv"
+  docker service update --with-registry-auth $srv --force -d
+done
