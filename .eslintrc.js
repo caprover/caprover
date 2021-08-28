e23b3c7d8af103c6c6dc4cea2973737daabf6e22
Kasra Bigdeli <kasra85@gmail.com>
2021-08-28 12:27:42 -0700

    Pass linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
@@ -9,15 +10,11 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
     ],
     "rules": {
-        "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-this-alias": "off",
         "@typescript-eslint/no-non-null-assertion": "off",
         "@typescript-eslint/no-unused-vars": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        // "@typescript-eslint/no-empty-interface": "off",
-        // "@typescript-eslint/no-var-requires": "off",
-        // "@typescript-eslint/ban-types": "off",
         "@typescript-eslint/ban-ts-comment": "off",
         "no-case-declarations": "off",
         "no-useless-escape": "off",
