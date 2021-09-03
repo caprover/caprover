51e76e789665fff5c0f3c8ddad18608b101d01e4
Kasra Bigdeli <kasra85@gmail.com>
2021-09-03 16:39:59 -0700

    Updated version

--- src/utils/CaptainConstants.ts
+++ src/utils/CaptainConstants.ts
@@ -17,7 +17,7 @@ const CONSTANT_FILE_OVERRIDE_USER =
 const configs = {
     publishedNameOnDockerHub: 'caprover/caprover',
 
-    version: '1.9.0',
+    version: '1.10.0',
 
     defaultMaxLogSize: '512m',
 
