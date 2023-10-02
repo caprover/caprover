fd80be267fc0cb0a613874de5269f6b874365b7d
Jeffrey Paul <408977+sneak@users.noreply.github.com>
2023-10-02 06:36:00 -0700

    remove nonconsensual spyware

--- src/user/events/emitter/AnalyticsLogger.ts
+++ src/user/events/emitter/AnalyticsLogger.ts
@@ -8,25 +8,9 @@ export class AnalyticsLogger extends IEventsEmitter {
         super()
     }
     isEventApplicable(event: ICapRoverEvent): boolean {
-        if (EnvVars.CAPROVER_DISABLE_ANALYTICS) {
-            return false
-        }
-
-        // some events aren't appropriate for usage stats
-        switch (event.eventType) {
-            case CapRoverEventType.AppBuildFailed:
-            case CapRoverEventType.AppBuildSuccessful:
-            case CapRoverEventType.UserLoggedIn: // perhaps anonymize the IP address and send it in the future
-                return false
-
-            case CapRoverEventType.InstanceStarted:
-            case CapRoverEventType.OneClickAppDetailsFetched:
-            case CapRoverEventType.OneClickAppListFetched:
-                return true
-        }
+        return false
     }
 
     emitEvent(event: ICapRoverEvent): void {
-        this.proManager.reportUnAuthAnalyticsEvent(event)
     }
 }
