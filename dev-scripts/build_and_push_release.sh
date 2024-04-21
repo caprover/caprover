1d6240811ba6d8e346aeb3303b690b483b12b6c1
Kasra Bigdeli <kasra85@gmail.com>
2024-04-20 22:59:14 -0700

    Updated packages

--- dev-scripts/build_and_push_release.sh
+++ dev-scripts/build_and_push_release.sh
@@ -44,7 +44,7 @@ echo $IMAGE_NAME:$CAPROVER_VERSION
 echo "**************************************"
 echo "**************************************"
 
-FRONTEND_COMMIT_HASH=7f857b4485be175a015845719b207fceeae1b636
+FRONTEND_COMMIT_HASH=2ef6d28fa67cb809e84250731d3ce54bd593c6f4
 
 ## Building frontend app
 ORIG_DIR=$(pwd)
