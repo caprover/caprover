5852e35e0ac924c1ffe302140518752906acc535
AriaieBOY <ariaieboy.ir@gmail.com>
2024-11-02 07:50:36 +0330

    bump nginx version to 1.27.2 (#2183)

--- src/utils/CaptainConstants.ts
+++ src/utils/CaptainConstants.ts
@@ -41,7 +41,7 @@ const configs = {
 
     appPlaceholderImageName: 'caprover/caprover-placeholder-app:latest',
 
-    nginxImageName: 'nginx:1.24',
+    nginxImageName: 'nginx:1.27.2',
 
     defaultEmail: 'runner@caprover.com',
 
