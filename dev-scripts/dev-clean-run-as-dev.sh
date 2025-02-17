78d11aeefb969dab0179c86d81be414ebb01c907
Kasra Bigdeli <kasra85@gmail.com>
2025-02-17 12:02:27 -0800

    Removed custom port from dev script

--- dev-scripts/dev-clean-run-as-dev.sh
+++ dev-scripts/dev-clean-run-as-dev.sh
@@ -5,7 +5,7 @@ if ! [ $(id -u) = 0 ]; then
    exit 1
 fi
 
-pwd > currentdirectory
+pwd >currentdirectory
 docker service rm $(docker service ls -q)
 sleep 1s
 docker secret rm captain-salt
@@ -17,11 +17,12 @@ chmod -R 777 /captain
 docker run \
    -e "CAPTAIN_IS_DEBUG=1" \
    -e "MAIN_NODE_IP_ADDRESS=127.0.0.1" \
-   -e "CAPTAIN_HOST_HTTP_PORT=10083" \
-   -e "CAPTAIN_HOST_HTTPS_PORT=10443" \
-   -e "CAPTAIN_HOST_ADMIN_PORT=13000" \
    -v /var/run/docker.sock:/var/run/docker.sock \
    -v /captain:/captain \
    -v $(pwd):/usr/src/app captain-debug
+
+# -e "CAPTAIN_HOST_HTTP_PORT=10083" \
+# -e "CAPTAIN_HOST_HTTPS_PORT=10443" \
+# -e "CAPTAIN_HOST_ADMIN_PORT=13000" \
 sleep 2s
 docker service logs captain-captain --follow
