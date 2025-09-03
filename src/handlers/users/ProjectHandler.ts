ef3a4db51fd2dc828f238163f7761dee0de5a40c
Kasra Bigdeli <kasra85@gmail.com>
2025-09-02 23:02:33 -0700

    updated project handler

---
+++ src/handlers/users/ProjectHandler.ts
@@ -0,0 +1,49 @@
+import { v4 as uuid } from 'uuid'
+import DataStore from '../../../datastore/DataStore'
+import { ProjectDefinition } from '../../../models/ProjectDefinition'
+import Logger from '../../../utils/Logger'
+
+export interface RegisterProjectParams {
+    name: string
+    parentProjectId: string
+    description: string
+}
+
+export interface BaseHandlerResult {
+    message: string
+}
+
+export interface RegisterProjectResult extends BaseHandlerResult {
+    data?: ProjectDefinition
+}
+
+export async function registerProject(
+    params: RegisterProjectParams,
+    dataStore: DataStore
+): Promise<RegisterProjectResult> {
+    const { name, parentProjectId, description } = params
+
+    Logger.d(`Creating project: ${name}`)
+
+    try {
+        const projectId = uuid()
+        const project = await dataStore
+            .getProjectsDataStore()
+            .saveProject(projectId, {
+                id: projectId,
+                name: name,
+                parentProjectId: parentProjectId,
+                description: description,
+            })
+
+        Logger.d(`Project created: ${name}`)
+
+        return {
+            message: `Project created: ${name}`,
+            data: project,
+        }
+    } catch (error: any) {
+        Logger.e(`Failed to create project: ${error}`)
+        throw error
+    }
+}
