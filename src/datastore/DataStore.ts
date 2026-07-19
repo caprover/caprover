7c7d2b33e20b22f3e5a17b158b5edac1826db927
Kasra Bigdeli <kasra85@gmail.com>
2026-07-18 22:27:18 -0700

    Harden service and volume naming migration

--- src/datastore/DataStore.ts
+++ src/datastore/DataStore.ts
@@ -60,6 +60,25 @@ const DEFAULT_NGINX_CONFIG_FOR_APP = fs
     .readFileSync(DEFAULT_NGINX_CONFIG_FOR_APP_PATH)
     .toString()
 
+export function runDataStoreMigrations(data: Configstore) {
+    const schemaVersion = data.get('schemaVersion') as number | undefined
+
+    if (schemaVersion && schemaVersion >= 2) {
+        return
+    }
+
+    const appDefinitions = data.get('appDefinitions')
+    if (appDefinitions) {
+        Object.keys(appDefinitions).forEach((appName) => {
+            const appDef = appDefinitions[appName] as IAppDefSaved
+            appDef.isLegacyAppName = true
+        })
+        data.set('appDefinitions', appDefinitions)
+    }
+
+    data.set('schemaVersion', 2)
+}
+
 class DataStore {
     private encryptor: CaptainEncryptor
     private namespace: string
@@ -78,19 +97,7 @@ class DataStore {
             }
         )
 
-        // running migrations
-        if (data.get('schemaVersion') < 2) {
-            const appDefinitions = data.get('appDefinitions')
-            if (appDefinitions) {
-                Object.keys(appDefinitions).forEach((appName) => {
-                    const appDef = appDefinitions[appName] as IAppDefSaved
-                    appDef.isLegacyAppName = true
-                })
-            }
-
-            data.set('appDefinitions', appDefinitions)
-            data.set('schemaVersion', 2)
-        }
+        runDataStoreMigrations(data)
 
         this.data = data
         this.namespace = namespace
