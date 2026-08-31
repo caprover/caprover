babbe2bf56349b6146daeaf54f59346e5d3be4f0
Kasra Bigdeli <kasra85@gmail.com>
2026-08-30 22:31:51 -0700

    Install jq for release validation

--- release/build-and-push.sh
+++ release/build-and-push.sh
@@ -48,6 +48,9 @@ if [ "$BRANCH" != "$EXPECTED_BRANCH" ]; then
     exit 1
 fi
 
+sudo apt-get update
+sudo apt-get install -y jq qemu-user-static
+
 if [ "$CHANNEL" = "release" ]; then
     CAPROVER_VERSION="$(./release/validate-version.sh "$IMAGE_NAME")"
 fi
@@ -71,7 +74,6 @@ echo "Building finished"
 cd "$ORIG_DIR"
 mv "$FRONTEND_DIR/caprover-frontend/build" ./dist-frontend
 
-sudo apt-get update && sudo apt-get install -y qemu-user-static
 # docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker run --rm --privileged tonistiigi/binfmt --install all
 # export DOCKER_CLI_EXPERIMENTAL=enabled
